56052f2fd4177e1a88608a794d812da15469190e
Kory <koryrallison@gmail.com>
2022-08-03 16:58:09 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 
 function App() {
+  <Menu />
   return (
     <div className="App">
       <Navbar dark color="primary">
